b9b9af4c1dfe0e6e1df1e6220c0b093a24801980
Fangxian <fangxian1108@gmail.com>
2023-10-15 16:06:46 +0800

    Update sha_padder_tb.sv
    sv read hex data from file

--- sha_padder_tb.sv
+++ sha_padder_tb.sv
@@ -4,12 +4,17 @@
 module sha_padder_tb;
 	logic [511:0] padded;
 	logic [23:0] message;
+	logic [511:0] data;
 
+	int fp;
 	sha_padder uut (.message(message), .padded(padded));
 
 	initial begin
     	$dumpfile("sha_padder_tb.vcd");
     	$dumpvars;
+	fb = $fopen("refyfx.seed","r+");
+	$fscanf(fb,"%h",data);
+	$display("%x", data);
     	assign message = 24'b011000010110001001100011;
     	#5;
     	$display("FINISHED block_tb");
